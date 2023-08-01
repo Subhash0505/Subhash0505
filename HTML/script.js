c51e1cf989534014d7908f408fe8ae0229add375
Subhash0505 <141058383+Subhash0505@users.noreply.github.com>
2023-08-01 20:03:27 +0530

    Add files via upload
    My first webpage created using HTML ,CSS and JavaScript.

---
+++ HTML/script.js
@@ -0,0 +1,20 @@
+document.getElementById("button").onclick = function(){
+    document.getElementById("text").innerHTML = "HELLO WORLD";    
+}   
+
+   
+
+document.getElementById("blue-circle").onclick = function(){
+    document.getElementById("blue-circle").style.display ="none";
+}
+document.getElementById("red-circle").onclick = function(){
+    document.getElementById("red-circle").style.display ="none";
+}
+document.getElementById("orange-circle").onclick = function(){
+    document.getElementById("orange-circle").style.display ="none";
+}
+document.getElementById("button2").onclick = function(){
+    document.getElementById("demo").innerHTML = Date();    
+}   
+
+
